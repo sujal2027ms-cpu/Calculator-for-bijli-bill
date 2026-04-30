8e890f870cb133a6c8ccac02406d0ff3371cf8dc
Monty-sharma <sujal2027ms@gmail.com>
2026-04-30 16:17:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function btn() {
+      let a = Number(document.getElementById('p1').value);
+      let b = Number(document.getElementById('p2').value);
+      let add = Math.abs(a-b)
+      let solve=add*10
+      document.getElementById('solve').innerText = "₹" + solve;}
+
+
